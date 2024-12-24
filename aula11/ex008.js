7e73b3ab4da103b37653e6d4ba97c488fb8a0b5c
vitor caetano <vitorcaetano505@gmail.com>
2024-12-23 23:41:00 -0300

    javascript

---
+++ aula11/ex008.js
@@ -0,0 +1,4 @@
+var vel =152
+console.log(`a velocidade do seu carro e ${vel}km/h`)
+if (vel > 60)
+    console.log('vc foi multado')
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'eua'
+if (pais == 'brasil') {
+    console.log('brasileiro')
+} else {
+    console.log('estrangeiro')
+}
