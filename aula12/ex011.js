bb5c4be09166fc814f4aad26073e8e4a0921248c
vitor caetano <vitorcaetano505@gmail.com>
2024-12-24 16:45:10 -0300

    tenho certeza q vou voltar aq bastante

---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 100
+if (idade < 16){
+    console.log('não vota')
+} else if ( idade < 18 || idade > 65) {
+        console.log("voto opcional")
+     } else if (idade >=18 ) {
+        console.log('voto obrigatorio')
+     }
